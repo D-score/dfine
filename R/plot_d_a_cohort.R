ee263c808a800950bd9a8a6857cc9ef551fbb9f2
Stef van Buuren <stef.vanbuuren@tno.nl>
2025-07-22 23:01:22 +0200

    Add daz and show_smooth arguments to plot_d_a_cohort()

--- R/plot_d_a_cohort.R
+++ R/plot_d_a_cohort.R
@@ -2,6 +2,8 @@
 #'
 #' This function plots D-score against age by cohort
 #' @param data Data on which model was fitted
+#' @param daz Logical indicating whether to plot D-score or DAZ.
+#' @param show_smooth Logical indicating whether to show a smoothed line
 #' @param file A filename to which the pdf is saved. If not specified, no
 #' plot is saved.
 #' @param col_manual A named vector of colors to use for the cohorts.
@@ -13,12 +15,15 @@
 #' @param ylim Limits for the y-axis.
 #' @param xbreaks Breaks for the x-axis.
 #' @param ybreaks Breaks for the y-axis.
+#' @param smooth_line_color Color of the smoothed line.
 #' @param device	Device to use. Can be either be a device function (e.g. png()),
 #'  or one of "eps", "ps", "tex" (pictex), "pdf", "jpeg", "tiff", "png", "bmp",
 #'  "svg" or "wmf" (windows only).
 #' @return A object of class \code{ggplot}
 #' @export
 plot_d_a_cohort <- function(data,
+                            daz = FALSE,
+                            show_smooth = FALSE,
                             file = NULL,
                             col_manual = NULL,
                             model_name = NULL,
@@ -29,6 +34,7 @@ plot_d_a_cohort <- function(data,
                             ylim = c(0, 90),
                             xbreaks = seq(0, 60, 12),
                             ybreaks = seq(0, 90, 20),
+                            smooth_line_color = "grey",
                             device = "pdf") {
 
   ref_name <- match.arg(ref_name)
@@ -58,8 +64,24 @@ plot_d_a_cohort <- function(data,
       select(.data$month, .data$SDM2:.data$SDP2) |>
       filter(.data$month <= 60) |>
       pivot_longer(names_to = "centile", values_to = "d", cols = -.data$month)
+  if (daz) {
+    references <- get_reference(population = ref_name) |>
+      mutate(month = .data$age * 12,
+             SDM2 = -2,
+             SDP2 = 2) |>
+      select(.data$month, .data$SDM2, .data$SDP2) |>
+      filter(.data$month <= 60) |>
+      pivot_longer(names_to = "centile", values_to = "d", cols = -.data$month)
+  }
 
   data$age <- data$a * 12
+  ylab <- "D-score ("
+  if (daz) {
+    data$d <- data$daz
+    ylab <- "DAZ ("
+    ylim <- ylim %||% c(-4, 4)
+    ybreaks <- ybreaks %||% seq(-3, 3, 1)
+  }
   color <- switch(ref_name,
                   preliminary_standards = "skyblue",
                   phase1 = "#C5EDDE",
@@ -67,15 +89,14 @@ plot_d_a_cohort <- function(data,
                   gcdg = "lightblue",
                   none = "transparent")
 
-  plot <-
-    ggplot(reference, aes(x = .data$month, y = .data$d, group = .data$centile)) +
+  plot <- ggplot(reference, aes(x = .data$month, y = .data$d, group = .data$centile)) +
     scale_colour_manual(values = col_manual %||% get_palette("cohort"),
                         na.value = "grey") +
     scale_x_continuous("Age (in months)",
                        limits = xlim,
                        breaks = xbreaks) +
     scale_y_continuous(
-      paste0("D-score (", model_name, ")"),
+      paste0(ylab, model_name, ")"),
       breaks = ybreaks,
       limits = ylim
     ) +
@@ -90,10 +111,29 @@ plot_d_a_cohort <- function(data,
       data = data,
       size = size,
       shape = shape
-    ) +
-    facet_wrap( ~ .data$cohort, ncol = 4) +
-    theme(legend.position = "none")
+    )
 
+  if (show_smooth) {
+    plot <- plot +
+      geom_smooth(
+        mapping = aes(
+          x = .data$age,
+          y = .data$d,
+          group = .data$cohort,
+          colour = .data$cohort
+        ),
+        data = data,
+        method = "loess",
+        se = FALSE,
+        span = 0.75,
+        color = smooth_line_color,
+        size = 1
+      )
+  }
+
+  plot <- plot +
+    facet_wrap(~ .data$cohort, ncol = 4) +
+    theme(legend.position = "none")
   n_facets <- length(unique(data$cohort))
   n_rows <- ceiling(n_facets / 4)
 
